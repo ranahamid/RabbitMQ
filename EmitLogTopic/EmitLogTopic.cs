847fae0793f1d4c0297d870d848aca07e9a00dba
Rana Hamid <rhamid@mymedicalhub.com>
2022-04-23 01:13:26 +0600

    dotnet run "kern.critical" "A critical kernel error"

---
+++ EmitLogTopic/EmitLogTopic.cs
@@ -0,0 +1,46 @@
+﻿//Working
+//dotnet run "kern.critical" "A critical kernel error"
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+//var qName = "My_Tasks";
+var exchangeName = "topic_logs";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic);
+
+var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+
+//channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+ 
+string message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+
+//var properties = channel.CreateBasicProperties();
+//properties.Persistent = true;
+
+channel.BasicPublish(
+    exchange: exchangeName,
+    routingKey: routingKey,
+    basicProperties: null,
+    body: body
+    );
+Console.WriteLine("[X] sebt {0}", message);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    var message = "Hello Eorld";
+    if (args.Length > 1)
+    {
+        message = string.Join(" ", args.Skip(1).ToArray());
+    }
+    return message;
+}
---
+++ ReceiveLogsTopic/ReceiveLogsTopic.cs
@@ -0,0 +1,54 @@
+﻿//dotnet run "kern.*" "*.critical"
+//dotnet run "*.critical"
+//dotnet run "kern.*"
+//dotnet run "#"
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+var exchangeName = "topic_logs";
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic);
+
+var queueName = channel.QueueDeclare(queue: "").QueueName;
+
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Usage: {0} [binding_key...]",
+                            Environment.GetCommandLineArgs()[0]);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+foreach (var bindingKey in args)
+{
+    channel.QueueBind(queue: queueName,
+                      exchange: exchangeName,
+                      routingKey: bindingKey);
+}
+ 
+Console.WriteLine("Waiting for message");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[X] Received: {message}, routing key:{ea.RoutingKey}");
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
