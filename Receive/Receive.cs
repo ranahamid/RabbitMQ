37696e72a60703fd30395cf7104e72c9d19f031f
Rana Hamid <rhamid@mymedicalhub.com>
2022-04-22 20:07:03 +0600

    Add project files.

---
+++ Receive/Receive.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare
+    (
+    queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+    );
+Console.WriteLine("Waiting for message");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body= ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[X] Received: {message}");
+};
+
+channel.BasicConsume(queue:"hello", autoAck:true, consumer:consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
