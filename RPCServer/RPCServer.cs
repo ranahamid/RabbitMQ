0ae7de287e4b0f5b3102e8ad443e6d2eecc68980
Rana Hamid <rhamid@mymedicalhub.com>
2022-04-23 02:30:15 +0600

    RPC

---
+++ RPCServer/RPCServer.cs
@@ -0,0 +1,74 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ var queueName = "rpc_queue";
+var exchangeName = "topic_logs";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+
+
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+Console.WriteLine("[X] Waiting RPC request.");
+
+consumer.Received += (model, ea) =>
+{
+    string response = String.Empty; 
+    var body = ea.Body.ToArray();
+    var props = ea.BasicProperties;
+    var replayProps = channel.CreateBasicProperties();
+    replayProps.CorrelationId = props.CorrelationId;
+
+    try
+    {
+        var message = Encoding.UTF8.GetString(body);
+        int n= int.Parse(message);
+        Console.WriteLine($"[X] Received: {message}, routing key:{ea.RoutingKey}");
+        response = fib(n).ToString();
+
+    }
+    catch (Exception ex)
+    {
+        response = "";
+    }
+    finally
+    {
+        var resBytes = Encoding.UTF8.GetBytes(response);
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: props.ReplyTo,
+            basicProperties: replayProps,
+            body: resBytes
+            );
+        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+    }
+};
+
+
+ 
+
+
+ 
+
+//var properties = channel.CreateBasicProperties();
+//properties.Persistent = true;
+ 
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static int fib(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+    return fib(n-1)+ fib(n - 2);
+}
---
+++ RpcClient/RpcClient.cs
@@ -0,0 +1,87 @@
+﻿//dotnet run "kern.*" "*.critical"
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Collections.Concurrent;
+using System.Text;
+
+public class RpcClient
+{
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly EventingBasicConsumer consumer;
+    private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+    private readonly IBasicProperties props;
+    public string  queueName = "rpc_queue";
+    public RpcClient()
+    {
+       
+
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+        };
+        var exchangeName = "topic_logs";
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        var replyQueueName = channel.QueueDeclare(queue: "").QueueName; 
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        var props = channel.CreateBasicProperties();      
+   var correlationId=Guid.NewGuid().ToString();
+        props.CorrelationId =
+  props.ReplyTo = replyQueueName;
+
+        
+       
+         
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            if(ea.BasicProperties.CorrelationId== correlationId)
+            {
+                respQueue.Add(message);
+            }
+            Console.WriteLine($"[X] Received: {message}, routing key:{ea.RoutingKey}");
+        };
+
+        channel.BasicConsume(queue: replyQueueName, autoAck: true, consumer: consumer);
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    public string Call(string message)
+    {
+        var resBytes = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: queueName,
+            basicProperties: props,
+            body: resBytes
+            );
+     return respQueue.Take();
+    }
+
+    public void Close()
+    {
+        connection.Close();
+    }
+}
+public class Rpc
+{
+    public static void Main()
+    {
+        var rpcClient = new RpcClient();
+        var response = rpcClient.Call("30");
+        Console.WriteLine($"[.] Got {response}");
+        rpcClient.Close();
+    }
+}
+
+ 
