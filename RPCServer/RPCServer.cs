83e4680ddbd184ea42a04508656faf1c4e94c6ed
Rana Hamid <rhamid@mymedicalhub.com>
2022-04-23 02:36:17 +0600

    s

--- RPCServer/RPCServer.cs
+++ RPCServer/RPCServer.cs
@@ -1,74 +1,76 @@
-﻿
+﻿using System;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
- var queueName = "rpc_queue";
-var exchangeName = "topic_logs";
-
-var factory = new ConnectionFactory() { HostName = "localhost" };
-using var connection = factory.CreateConnection();
-using var channel = connection.CreateModel();
-
-channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
-
-
-
-var consumer = new EventingBasicConsumer(channel);
-channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
-
-Console.WriteLine("[X] Waiting RPC request.");
-
-consumer.Received += (model, ea) =>
+class RPCServer
 {
-    string response = String.Empty; 
-    var body = ea.Body.ToArray();
-    var props = ea.BasicProperties;
-    var replayProps = channel.CreateBasicProperties();
-    replayProps.CorrelationId = props.CorrelationId;
-
-    try
+    public static void Main()
     {
-        var message = Encoding.UTF8.GetString(body);
-        int n= int.Parse(message);
-        Console.WriteLine($"[X] Received: {message}, routing key:{ea.RoutingKey}");
-        response = fib(n).ToString();
+        var queueName = "rpc_queue";
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: queueName, durable: false,
+              exclusive: false, autoDelete: false, arguments: null);
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queue: queueName,
+              autoAck: false, consumer: consumer);
+            Console.WriteLine(" [x] Awaiting RPC requests");
 
+            consumer.Received += (model, ea) =>
+            {
+                string response = null;
+
+                var body = ea.Body.ToArray();
+                var props = ea.BasicProperties;
+                var replyProps = channel.CreateBasicProperties();
+                replyProps.CorrelationId = props.CorrelationId;
+
+                try
+                {
+                    var message = Encoding.UTF8.GetString(body);
+                    int n = int.Parse(message);
+                    Console.WriteLine(" [.] fib({0})", message);
+                    response = fib(n).ToString();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(" [.] " + e.Message);
+                    response = "";
+                }
+                finally
+                {
+                    var responseBytes = Encoding.UTF8.GetBytes(response);
+                    channel.BasicPublish(exchange: "", routingKey: props.ReplyTo,
+                      basicProperties: replyProps, body: responseBytes);
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                      multiple: false);
+                }
+            };
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
     }
-    catch (Exception ex)
+
+    /// 
+
+    /// Assumes only valid positive integer input.
+    /// Don't expect this one to work for big numbers, and it's
+    /// probably the slowest recursive implementation possible.
+    /// 
+
+    private static int fib(int n)
     {
-        response = "";
+        if (n == 0 || n == 1)
+        {
+            return n;
+        }
+
+        return fib(n - 1) + fib(n - 2);
     }
-    finally
-    {
-        var resBytes = Encoding.UTF8.GetBytes(response);
-        channel.BasicPublish(
-            exchange: "",
-            routingKey: props.ReplyTo,
-            basicProperties: replayProps,
-            body: resBytes
-            );
-        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-    }
-};
-
-
- 
-
-
- 
-
-//var properties = channel.CreateBasicProperties();
-//properties.Persistent = true;
- 
-
-Console.WriteLine(" Press [enter] to exit.");
-Console.ReadLine();
-
-static int fib(int n)
-{
-    if (n == 0 || n == 1)
-        return n;
-    return fib(n-1)+ fib(n - 2);
 }
