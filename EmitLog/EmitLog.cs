8754ff1ede099ac593de4c58a9e18a0169db9563
Rana Hamid <rhamid@mymedicalhub.com>
2022-04-22 21:33:30 +0600

    emit logs

---
+++ EmitLog/EmitLog.cs
@@ -0,0 +1,49 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+//var qName = "My_Tasks";
+var exchangeName = "logs";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+
+
+//channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+var list = new string[5];
+list[0] = "HLatest RHHH  changes ello ll..";
+list[1] = "HLatest RHHH changes ello kk.00.";
+list[2] = "HLatest RHHH changes ello oo..";
+list[3] = "HLatest RHHH changes ello TT..";
+list[4] = "HLatest RHHH changes ello PP..";
+string message = GetMessage(list);
+var body = Encoding.UTF8.GetBytes(message);
+
+//var properties = channel.CreateBasicProperties();
+//properties.Persistent = true;
+
+channel.BasicPublish(
+    exchange: exchangeName,
+    routingKey: "",
+    basicProperties: null,
+    body: body
+    );
+Console.WriteLine("[X] sebt {0}", message);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    var message = "Hello Eorld";
+    if (args.Length > 0)
+    {
+        message = string.Join("U+002CU+0020", args);
+    }
+    return message;
+}
---
+++ ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+//var qName = "My_Tasks";
+var exchangeName = "logs";
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+
+var queueName = channel.QueueDeclare().QueueName;
+channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "");
+
+//channel.QueueDeclare
+//    (
+//    queue: qName,
+//    durable: true, //durable
+//    exclusive: false,
+//    autoDelete: false,
+//    arguments: null
+//    );
+//channel.BasicQos(
+//    prefetchSize: 0,
+//    prefetchCount: 1,
+//    global: false
+//    );
+Console.WriteLine("Waiting for message");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[X] Received: {message}");
+
+    int dots = message.Split('.').Length - 1;
+    Thread.Sleep(dots * 1000);
+    Console.WriteLine("[X] done");
+
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(
+    queue: queueName,
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
